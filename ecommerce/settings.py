1b9142028d18012bd45759e63f8f6d633b7700ab
Ebenezer Gakuo <gakuoebenezer@gmail.com>
2021-05-18 20:32:51 +0300

    models created

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -78,11 +78,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecom',
+        'USER': 'ebay',
+        'PASSWORD': 'fella',
+        'HOST': 'localhost','127.0.0.1'
+        'PORT': '',
     }
 }
-
+ALLOWED_HOSTS = ['127.0.0.1',]
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
